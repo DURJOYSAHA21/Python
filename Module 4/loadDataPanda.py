ad221b814eda1f1967b13461927e8dd29f56482f
Durjoy Saha <durjoy.saha1115@gmail.com>
2026-03-12 16:06:25 +0600

    from slice

---
+++ Module 4/loadDataPanda.py
@@ -0,0 +1,18 @@
+import pandas as pd
+print(pd.read_csv("data.csv"))
+
+df = pd.read_csv("data.csv")
+print(df.head())
+
+data={
+    "Artist": ["Michael Jackson", "AC/DC"],
+    "Released": [1982, 1980]
+}
+df1=pd.DataFrame(data)
+print(df1)
+print("\n")
+print(df1["Artist"])
+print("\n")
+print(df1.iloc[0,0]) #position based search
+
+print(df1.loc[0,"Artist"])
